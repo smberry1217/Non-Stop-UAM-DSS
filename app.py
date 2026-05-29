1cb0ec4ad0434128ea32938f5f29a9e3f01fd517
smberry1217 <88547480+smberry1217@users.noreply.github.com>
2026-05-29 11:35:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.markdown("""
         margin-bottom: 15px;
     }
     </style>
-""", unsafe_allow_allowed_html=True)
+""", unsafe_allow_html=True)
 
 # --- 시스템 고정 상수 ---
 BASE_COST_CAPEX = 7707.5   
